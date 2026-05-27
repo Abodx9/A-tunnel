f60efee0afe5e1e7a01f28af3c5aa0a88e30a938
Abodx9 <abodx@gmx.de>
2026-05-27 23:03:46 +0200

    feat: Expose the code to root

---
+++ src/atunnel/__init__.py
@@ -0,0 +1,9 @@
+"""
+atunnel: A minimal Python package to expose localhost to the public internet
+using Cloudflare's cloudflared tunnel (quick tunnels).
+"""
+
+from atunnel.tunnel import Tunnel
+
+__all__ = ["Tunnel"]
+__version__ = "1.0.0"
